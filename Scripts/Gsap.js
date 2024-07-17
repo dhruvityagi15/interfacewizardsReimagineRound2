0043f7ab5a3ee95e5d6c8b59551a7c3c00ae9fe6
Anubhav <121851863+Anubhavprogramer@users.noreply.github.com>
2024-07-17 13:15:30 +0530

    updates

--- Scripts/Gsap.js
+++ Scripts/Gsap.js
@@ -1,10 +1,21 @@
+// Debounce function to ensure click events are handled once within a certain timeframe
+function debounce(func, delay) {
+    let timer;
+    return function() {
+        clearTimeout(timer);
+        timer = setTimeout(func, delay);
+    };
+}
+
 window.addEventListener('load', function() {
     Loading();
 });
 
-function Loading(){
-    tl = gsap.timeline();
-    tl.from('.ball',{
+function Loading() {
+    gsap.registerPlugin();
+    var tl = gsap.timeline();
+    
+    tl.from('.ball', {
         duration: 2,
         y: -1000,
         ease: 'bounce',
@@ -12,23 +23,64 @@ function Loading(){
         squash: 5,
         squashID: "myBounce-squash",
     })
-    .to('.ball',{
+    .to('.ball', {
         width: 1000,
-        height:7,
+        height: 7,
         ease: "power4.out",
-        duration:0.7,
-        delay:0.3
+        duration: 0.7,
+        delay: 0.3
     })
-    .to('.ball',{
-        y:200,
-        duration:  0.5,
+    .to('.ball', {
+        y: 200,
+        duration: 0.5,
         ease: 'power4.out',
         opacity: 0,
         delay: 0.5
-    },'a')
-    .to('.loading_text',{
-        height:200,
+    }, 'a')
+    .to('.loading_text', {
+        height: 200,
         duration: 0.5,
-        delay: 0.5
-    },'a')
+        delay: 0.5,
+        onComplete: function() {
+            tl.kill();
+            init();
+        }
+    }, 'a');
 }
+
+function init(){
+    document.querySelector(".Loading").classList.add("hidden");
+    document.querySelector(".main").classList.remove("hidden");
+
+    menu_func();
+}
+
+function menu_func(){
+    var menu = document.querySelector('#menu');
+    var tl = gsap.timeline({ paused: true }); // Initialize timeline paused
+    
+    tl.to('.full', {
+        right: 0,
+        duration: 0.6,
+        ease: 'power4.in',
+    });
+    tl.from('.full h4', {
+        x: 100,
+        stagger: 0.24,
+        duration: 0.6,
+        opacity: 0,
+    });
+    
+    menu.addEventListener('click', debounce(function() {
+        if (!tl.isActive()) { // Check if timeline is not actively animating
+            if (tl.progress() === 1) {
+                tl.reverse();
+                console.log('Menu closed');
+            } else {
+                tl.play();
+                console.log('Menu opened');
+            }
+        }
+    }, 300)); // Adjust debounce delay as needed (e.g., 300ms)
+}
+
